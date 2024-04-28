aceffa8920cb59a7e171da9465a91826513a03eb
TirthNandha <ntirth15@gmail.com>
2024-04-28 12:14:23 +0530

    implemented mapping function

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,13 +2,22 @@ import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
+import notes from "../notes"
+
+
 
 function App() {
     return (
       <div>
         <Header />
+        {notes.map(noteEntry => ( 
+          <Note 
+            key = {noteEntry.key}
+            title= {noteEntry.title}
+            content= {noteEntry.content}
+          />)
+        )}
         <Footer />
-        <Note />
       </div>
     );
   }
