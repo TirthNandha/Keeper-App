945356e66563849b62028651b32e44ac1bff9777
TirthNandha <ntirth15@gmail.com>
2024-05-28 15:38:54 +0530

    Add loading spinner to App component

--- keeper-fe/src/Components/App.jsx
+++ keeper-fe/src/Components/App.jsx
@@ -57,7 +57,17 @@ function App() {
         <Header />
         <CreateArea onAdd={addNote} onDeleteAll={handleDeleteAll} notesLength={notes.length}/>
         {isLoading?(
-          <div>PLease wait... The server is loading...</div>
+          <div>
+
+            <div>PLease wait... The server is loading...</div>
+          <div className="spinner">
+            <div className="rect1"></div>
+            <div className="rect2"></div>
+            <div className="rect3"></div>
+            <div className="rect4"></div>
+            <div className="rect5"></div>
+          </div>
+          </div>
         ): (
           <ul>
             {notes.map((entry, index) => {
